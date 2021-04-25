9a6f6ab4d8ac6a722fcc7c611b7c116c4f3f9492
Difen Yedidya <46747165+DifenYedidya@users.noreply.github.com>
2021-04-25 20:50:23 +0700

    Add files via upload
    Routing with 9 files

--- Desain-web-bimbel-avicenna/script/script.js
+++ Desain-web-bimbel-avicenna/script/script.js
@@ -8,19 +8,3 @@ function scrollFunction() {
     document.getElementById("pageHeader").style.padding = "20px 100px";
   }
 }
-
-//show clicked content with Jquery
-var $content = $(".menuContent");
-
-function showContent(selector) {
-  $content.hide();
-  $(selector).show();
-}
-
-$(".menu").on("click", ".menuBtn", function (e) {
-  showContent(e.currentTarget.hash);
-  e.preventDefault();
-});
-
-showContent("#home");
-
